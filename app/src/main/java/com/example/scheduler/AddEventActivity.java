aa8f02d95a84763f38119d7be6ccf4083f9718de
fishlicker <a3xue@edu.uwaterloo.ca>
2017-08-27 22:50:33 -0400

    added images for buttons, changed events to be independant of date

--- app/src/main/java/com/example/scheduler/AddEventActivity.java
+++ app/src/main/java/com/example/scheduler/AddEventActivity.java
@@ -14,10 +14,18 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class AddEventActivity extends AppCompatActivity implements View.OnClickListener{
     String day_string[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+    boolean edit = false;
+    String event_id;
+    int edit_num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,27 +47,59 @@ public class AddEventActivity extends AppCompatActivity implements View.OnClickL
 
         Intent intent = getIntent();
         if (intent.hasExtra("edit")){
+            Log.d("debug","editing");
+            edit = true;
+            event_id = intent.getStringExtra("edit");
+            edit_num = intent.getIntExtra("number",0);
             String file_name = intent.getStringExtra("edit");
             event edit_event = event.retreivedata(this,file_name);
+            if (edit_event==null){
+                Log.d("debug","uh oh");
+            }
 
             EditText event_name = (EditText)findViewById(R.id.event_name);
             event_name.setText(edit_event.name);
 
-            EditText event_starttime_hour = (EditText)findViewById(R.id.event_name);
-            event_starttime_hour.setText(edit_event.starttime_hour);
+            EditText event_starttime_hour = (EditText)findViewById(R.id.event_starttime_hour);
+            event_starttime_hour.setText(String.valueOf(edit_event.starttime_hour));
 
-            EditText event_starttime_min = (EditText)findViewById(R.id.event_name);
-            event_starttime_min.setText(edit_event.starttime_min);
+            EditText event_starttime_min = (EditText)findViewById(R.id.event_starttime_min);
+            event_starttime_min.setText(String.valueOf(edit_event.starttime_min));
 
-            event_starttime_am.setText(edit_event.starttime_hour);
+            event_starttime_am.setText(edit_event.starttime_am);
 
-            EditText event_endtime_hour = (EditText)findViewById(R.id.event_name);
-            event_endtime_hour.setText(edit_event.endtime_hour);
+            EditText event_endtime_hour = (EditText)findViewById(R.id.event_endtime_hour);
+            event_endtime_hour.setText(String.valueOf(edit_event.endtime_hour));
 
-            EditText event_endtime_min = (EditText)findViewById(R.id.event_name);
-            event_endtime_min.setText(edit_event.endtime_min);
+            EditText event_endtime_min = (EditText)findViewById(R.id.event_endtime_min);
+            event_endtime_min.setText(String.valueOf(edit_event.endtime_min));
 
-            event_endtime_am.setText(edit_event.starttime_hour);
+            event_endtime_am.setText(edit_event.endtime_am);
+
+            for (int i=0;i<7;i++){
+                if (edit_event.day[i]){
+                    String buttonID = "event_check_" + i;
+                    int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                    CheckBox day = (CheckBox) findViewById(resID);
+                    day.setChecked(true);
+                }
+            }
+
+            EditText event_desc = (EditText) findViewById(R.id.event_desc);
+            event_desc.setText(edit_event.desc);
+
+            EditText event_location = (EditText) findViewById(R.id.event_desc);
+            event_location.setText(edit_event.location);
+
+            if (edit_event.weekly){
+                CheckBox weekly = (CheckBox) findViewById(R.id.event_weekly);
+                weekly.setChecked(true);
+            }
+            else{
+                CheckBox one_time = (CheckBox) findViewById(R.id.event_one_time);
+                one_time.setChecked(true);
+            }
+
         }
     }
 
@@ -168,8 +208,8 @@ public class AddEventActivity extends AppCompatActivity implements View.OnClickL
             }
 
             for (int i=0;i<7;i++){
-                SharedPreferences shared = this.getSharedPreferences(day_string[i], Context.MODE_PRIVATE);
-                if (shared.getInt(day_string[i]+"_num_events",0)>=10){
+                SharedPreferences shared = this.getSharedPreferences("events", Context.MODE_PRIVATE);
+                if (shared.getInt(day_string[i]+"_num_events",0)>=7){
                     event_error.setText("TOO MANY EVENTS ON "+day_string[i]+" ALREADY");
                     return;
                 }
@@ -189,7 +229,22 @@ public class AddEventActivity extends AppCompatActivity implements View.OnClickL
                     one_time);
 
             event_error.setText("DONE");
-            new_event.saveData(this);
+            if (!edit) new_event.saveData(this);
+            else{
+                new_event.event_num = edit_num;
+                try {
+                    File file = this.getFileStreamPath(event_id);
+                    file.delete();
+                    FileOutputStream fos = this.openFileOutput(event_id, Context.MODE_PRIVATE);
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(new_event);
+                    oos.close();
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d("debug","exception");
+                }
+            }
 
             startActivity(new Intent(this, MainActivity.class));
         }
